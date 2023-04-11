5f59b4104229a4dcf62f0fa07bd1811ab93576b1
Blair Sonnek <blairsonnek@hotmail.com>
2023-04-11 08:57:21 -0500

    NetApp Data

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -10,7 +10,7 @@ from text import nonewlines
 class RetrieveThenReadApproach(Approach):
 
     template = \
-"You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions. " + \
+"You are an intelligent assistant helping customers answer questions about a product and how to solve product issue questions. " + \
 "Use 'you' to refer to the individual asking the questions even if they ask with 'I'. " + \
 "Answer the following question using only the data provided in the sources below. " + \
 "For tabular information return it as an html table. Do not return markdown format. "  + \
@@ -19,16 +19,15 @@ class RetrieveThenReadApproach(Approach):
 """
 
 ###
-Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
+Question: 'What is VDS?'
 
 Sources:
-info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
-info2.pdf: Overlake is in-network for the employee plan.
-info3.pdf: Overlake is the name of the area that includes a park and ride near Bellevue.
-info4.pdf: In-network institutions include Overlake, Swedish and others in the region
+
+Virtual_Desktop_Service_Documentation-176.pdf: Overlake is in-network for the employee plan.
+
 
 Answer:
-In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf].
+It appears to be a service that provides virtual desktops and application management for users, with features such as user management, application deployment, and password resets [Virtual_Desktop_Service_Documentation-176.pdf].
 
 ###
 Question: '{q}'?
--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Summarize key E-Series Terminology?",
+        value: "Summarize key E-Series Terminology?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What are supported adjustable support rails for a controller-drive tray?", value: "What are supported adjustable support rails for a controller-drive tray?" },
+    { text: "What are a few upgrade considerations before upgrading controllers?", value: "What are a few upgrade considerations before upgrading controllers?" }
 ];
 
 interface Props {
