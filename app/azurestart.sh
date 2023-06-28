cd38ad3f39f48a6d556250b4f5394872d6fa5d2d
Blair Sonnek <blairsonnek@hotmail.com>
2023-06-28 17:02:03 -0500

    Update azurestart.sh

--- app/azurestart.sh
+++ app/azurestart.sh
@@ -1,12 +1,8 @@
-cd app/frontend
-npm install
-if [ $? -ne 0 ]; then
-    echo "Failed to restore frontend npm packages"
-    exit $?
-fi
 
-cd ../backend
-./venv/bin/python ./app.py
+
+cd app/backend
+./backend_env/bin/python ./backend_env/bin/activate
+./backend_env/bin/python ./app.py
 if [ $? -ne 0 ]; then
     echo "Failed to start backend"
     exit $?
